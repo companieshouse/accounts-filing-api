ce4248980a13f4133157b533062e6303e0d6a8ff
wdhda-ch <awoodhead@companieshouse.gov.uk>
2023-12-01 13:24:33 +0000

    Add in PackageType

---
+++ src/main/java/uk/gov/companieshouse/accounts/filing/model/enums/PackageType.java
@@ -0,0 +1,44 @@
+package uk.gov.companieshouse.accounts.filing.model.enums;
+
+import java.util.Optional;
+
+public enum PackageType {
+
+    UKSEF("UKSEF"),
+    WELSH("Welsh"),
+    CIC("CIC"),
+    LIMITED_PARTNERSHIP("Limited Partnership"),
+    GROUP_PACKAGE_400("Group Package 400"),
+    GROUP_PACKAGE_401("Group Package 401"),
+    OVERSEAS("overseas"),
+    AUDIT_EXEMPT_SUBSIDIARY("Audit Exempt Subsidiary"),
+    FILING_EXEMPT_SUBSIDIARY("Filing Exempt Subsidiary");
+    ;
+
+    private final String type;
+
+    private PackageType (String type) {
+        this.type = type;
+    }
+
+    private String lowerCase(){
+        return this.type.toLowerCase();
+    }
+
+    public static Optional<PackageType> findPackageType(String packageString) {
+
+        if(packageString == null){
+            return Optional.empty();
+        }
+
+        String formattedPackageString = packageString.strip().toLowerCase();
+        for (PackageType packageType : PackageType.values()) {
+            if (packageType.lowerCase().contentEquals(formattedPackageString))
+            {
+                return Optional.of(packageType);
+            }
+        }
+        return Optional.empty();
+    }
+}
+
