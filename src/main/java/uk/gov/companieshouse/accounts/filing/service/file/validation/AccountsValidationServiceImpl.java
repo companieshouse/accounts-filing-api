871fafa9e1e7af368604be3cf110b8acaa2c0b23
wdhda-ch <awoodhead@companieshouse.gov.uk>
2023-12-06 13:22:36 +0000

    Fix spacing

--- src/main/java/uk/gov/companieshouse/accounts/filing/service/file/validation/AccountsValidationServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/accounts/filing/service/file/validation/AccountsValidationServiceImpl.java
@@ -17,83 +17,81 @@ import uk.gov.companieshouse.api.model.ApiResponse;
 import uk.gov.companieshouse.api.model.accountvalidator.AccountsValidatorStatusApi;
 import uk.gov.companieshouse.logging.Logger;
 
-
 @Component
 public class AccountsValidationServiceImpl implements AccountsValidationService {
 
-  private final Logger logger;
-  private final AccountsFilingRepository requestFilingRepository;
-  private final AccountsValidatorAPI accountsValidatorAPI;
+    private final Logger logger;
+    private final AccountsFilingRepository requestFilingRepository;
+    private final AccountsValidatorAPI accountsValidatorAPI;
 
-  @Autowired
-  public AccountsValidationServiceImpl(
-      Logger logger,
-      AccountsFilingRepository requestFilingRepository,
-      AccountsValidatorAPI accountsValidatorAPI) {
-    this.logger = logger;
-    this.requestFilingRepository = requestFilingRepository;
-    this.accountsValidatorAPI = accountsValidatorAPI;
-  }
-
-
-  @Override
-  public Optional<AccountsValidatorStatusApi> validationStatusResult(final String fileId) {
-    ApiResponse<AccountsValidatorStatusApi> response = accountsValidatorAPI.getValidationCheck(fileId);
-    HttpStatus status = HttpStatus.resolve(response.getStatusCode());
-    switch (Objects.requireNonNull(status)) {
-      case NOT_FOUND:
-        return Optional.empty();
-      case OK:
-        return Optional.ofNullable(response.getData());
-      default:
-        var message = "Unexpected response status from account validator api when getting file details.";
-        
-        logger.errorContext(fileId, message, null, errorMessageMap(Map.of(
-          "expected", "200 or 404",
-          "status", response.getStatusCode()
-          )));
-        
-          throw new ResponseException(message);
-    }
-  }
-
-  @Override
-  public void saveFileValidationResult(AccountsFilingEntry accountsFilingEntry, AccountsValidatorStatusApi accountStatus) {
-    String fileId = accountStatus.fileId();
-    String accountsType = accountStatus.resultApi().data().accountType();
-
-    accountsFilingEntry.setAccountsType(accountsType);
-    accountsFilingEntry.setFileId(fileId);
-
-    requestFilingRepository.save(accountsFilingEntry);
-
-    var message = String.format("Account filing id: %s has been updated to include file id: %s with account type: %s",
-                                accountsFilingEntry.getAccountsFilingId(), fileId, accountsType);
-    logger.debug(message);
-  }
-
-
-  @Override
-  public AccountsFilingEntry getFilingEntry(String accountsFilingId) {
-    Optional<AccountsFilingEntry> filingEntry = requestFilingRepository.findById(accountsFilingId);
-    if (filingEntry.isPresent()) {
-        return filingEntry.get();
+    @Autowired
+    public AccountsValidationServiceImpl(
+            Logger logger,
+            AccountsFilingRepository requestFilingRepository,
+            AccountsValidatorAPI accountsValidatorAPI) {
+        this.logger = logger;
+        this.requestFilingRepository = requestFilingRepository;
+        this.accountsValidatorAPI = accountsValidatorAPI;
     }
 
-    var message = "document not found";
+    @Override
+    public Optional<AccountsValidatorStatusApi> validationStatusResult(final String fileId) {
+        ApiResponse<AccountsValidatorStatusApi> response = accountsValidatorAPI.getValidationCheck(fileId);
+        HttpStatus status = HttpStatus.resolve(response.getStatusCode());
+        switch (Objects.requireNonNull(status)) {
+            case NOT_FOUND:
+                return Optional.empty();
+            case OK:
+                return Optional.ofNullable(response.getData());
+            default:
+                var message = "Unexpected response status from account validator api when getting file details.";
+
+                logger.errorContext(fileId, message, null, errorMessageMap(Map.of(
+                        "expected", "200 or 404",
+                        "status", response.getStatusCode())));
+                throw new ResponseException(message);
+        }
+    }
+
+    @Override
+    public void saveFileValidationResult(AccountsFilingEntry accountsFilingEntry,
+            AccountsValidatorStatusApi accountStatus) {
+        String fileId = accountStatus.fileId();
+        String accountsType = accountStatus.resultApi().data().accountType();
+
+        accountsFilingEntry.setAccountsType(accountsType);
+        accountsFilingEntry.setFileId(fileId);
+
+        requestFilingRepository.save(accountsFilingEntry);
+
+        var message = String.format(
+                "Account filing id: %s has been updated to include file id: %s with account type: %s",
+                accountsFilingEntry.getAccountsFilingId(), fileId, accountsType);
+        logger.debug(message);
+    }
+
+    @Override
+    public AccountsFilingEntry getFilingEntry(String accountsFilingId) {
+        Optional<AccountsFilingEntry> filingEntry = requestFilingRepository.findById(accountsFilingId);
+        if (filingEntry.isPresent()) {
+            return filingEntry.get();
+        }
+
+        var message = "document not found";
         logger.errorContext(accountsFilingId, message, null, errorMessageMap(Map.of(
-              "expected", "accountsFilingId",
-              "actual", accountsFilingId )));
+                "expected", "accountsFilingId",
+                "actual", accountsFilingId)));
         throw new EntryNotFoundException(message);
-  }
+    }
 
-  /**
-   * Makes immutable map; a mutable map.
-   * @param immutableMap
-   * @return
-   */
-  private Map<String, Object> errorMessageMap(Map<String, Object> immutableMap){
-    return new HashMap<>(immutableMap);
-  }
+    /**
+     * Makes immutable map; a mutable map.
+     * 
+     * @param immutableMap
+     * @return
+     */
+    private Map<String, Object> errorMessageMap(Map<String, Object> immutableMap) {
+        return new HashMap<>(immutableMap);
+    }
 
 }
