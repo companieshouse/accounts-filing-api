0f2c90a5c1a993122f49794b4948fa7be5c1b77a
wdhda-ch <awoodhead@companieshouse.gov.uk>
2023-12-18 15:45:17 +0000

    Update format

--- src/main/java/uk/gov/companieshouse/accounts/filing/service/transaction/TransactionServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/accounts/filing/service/transaction/TransactionServiceImpl.java
@@ -37,15 +37,15 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public Optional<Transaction> getTransaction(final String transactionId) throws NullPointerException {
-        
-        try{
+
+        try {
             final ApiResponse<Transaction> response = transactionAPI.get(transactionId);
             return Optional.ofNullable(response.getData());
-        } catch (ApiErrorResponseException e){
+        } catch (ApiErrorResponseException e) {
             int statusCode = e.getStatusCode();
             final HttpStatus status = HttpStatus.resolve(statusCode);
 
-            if(Objects.requireNonNull(status) == HttpStatus.NOT_FOUND) {
+            if (Objects.requireNonNull(status) == HttpStatus.NOT_FOUND) {
                 return Optional.empty();
             } else {
                 throw getTransactionThrowableExceptions(transactionId, status);
@@ -56,56 +56,52 @@ public class TransactionServiceImpl implements TransactionService {
         }
     }
 
-    
     @Override
     public void updateTransaction(final Transaction transaction) throws NullPointerException {
         final var message = "Unexpected response status when updating transaction.";
         final var externalIssueMessage = "External service issue blocked updating transaction.";
-        try{
+        try {
             final ApiResponse<Void> response = transactionAPI.patch(transaction);
             HttpStatus status = Objects.requireNonNull(HttpStatus.resolve(response.getStatusCode()));
-            
-            if(status != HttpStatus.NO_CONTENT) {
+
+            if (status != HttpStatus.NO_CONTENT) {
                 logger.errorContext(transaction.getId(), message, null, ImmutableConverter.toMutableMap(Map.of(
-                    EXPECTED, "204",
-                    STATUS, status.value()
-                    )));
-                    throw new ResponseException(message);
-                }
-                
-            } catch (ApiErrorResponseException e){
-                final HttpStatus status = Objects.requireNonNull(HttpStatus.resolve(e.getStatusCode()));
-                
-                logger.errorContext(transaction.getId(), message, null, ImmutableConverter.toMutableMap(Map.of(
-                    EXPECTED, "204",
-                    STATUS, status.value()
-                    )));
-                    
-                    if (status == HttpStatus.INTERNAL_SERVER_ERROR){
-                        logger.errorContext(transaction.getId(), externalIssueMessage, null, new HashMap<>());
-                        throw new ExternalServiceException(externalIssueMessage);
-                    } else {
-                        throw new ResponseException(message);
-                    }
-                    
-                } catch (URIValidationException e) {
-                    throw new UriValidationException(e);
-                }
+                        EXPECTED, "204",
+                        STATUS, status.value())));
+                throw new ResponseException(message);
             }
+
+        } catch (ApiErrorResponseException e) {
+            final HttpStatus status = Objects.requireNonNull(HttpStatus.resolve(e.getStatusCode()));
+
+            logger.errorContext(transaction.getId(), message, null, ImmutableConverter.toMutableMap(Map.of(
+                    EXPECTED, "204",
+                    STATUS, status.value())));
+
+            if (status == HttpStatus.INTERNAL_SERVER_ERROR) {
+                logger.errorContext(transaction.getId(), externalIssueMessage, null, new HashMap<>());
+                throw new ExternalServiceException(externalIssueMessage);
+            } else {
+                throw new ResponseException(message);
+            }
+
+        } catch (URIValidationException e) {
+            throw new UriValidationException(e);
+        }
+    }
 
     private RuntimeException getTransactionThrowableExceptions(final String transactionId, final HttpStatus status) {
         final var message = "Unexpected response status when getting transaction.";
         final var externalIssueMessage = "External issue blocked getting transaction.";
         logger.errorContext(transactionId, message, null, ImmutableConverter.toMutableMap(Map.of(
-            EXPECTED, "200 or 404",
-            STATUS, status
-        )));
-    
-        if(status == HttpStatus.INTERNAL_SERVER_ERROR){
+                EXPECTED, "200 or 404",
+                STATUS, status)));
+
+        if (status == HttpStatus.INTERNAL_SERVER_ERROR) {
             return new ExternalServiceException(externalIssueMessage);
         } else {
             return new ResponseException(message);
         }
     }
-            
+
 }
