5463a49fa0eff880200ad3a6305408f25d922451
amartin7663 <almartin@companieshouse.gov.uk>
2024-04-24 14:56:33 +0100

    add missing eric variable

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -93,13 +93,13 @@ module "ecs-service" {
   use_set_environment_files = local.use_set_environment_files
 
   # Eric variables
-  use_eric_reverse_proxy  = local.use_eric_reverse_proxy
-  eric_port               = local.eric_port
-  eric_version            = local.eric_version
-  eric_cpus               = var.required_cpus
-  eric_memory             = var.required_memory
-  eric_environment        = local.eric_environment
-  eric_secrets            = local.eric_secrets
+  use_eric_reverse_proxy    = local.use_eric_reverse_proxy
+  eric_port                 = local.eric_port
+  eric_version              = local.eric_version
+  eric_cpus                 = var.required_cpus
+  eric_memory               = var.required_memory
+  eric_environment_filename = local.eric_environment_filename
+  eric_secrets              = local.eric_secrets
 
   depends_on = [module.secrets]
 }
