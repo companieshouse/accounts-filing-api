96e1f1698901ec521bb339153c34251149e1d7c7
ch-skathirvel <skathirvel@companieshouse.gov.uk>
2024-05-21 11:54:50 +0100

    JIRA : AOAF-45 Description : Updated test cases

--- src/test/java/uk/gov/companieshouse/accounts/filing/transformer/TransformerServiceTest.java
+++ src/test/java/uk/gov/companieshouse/accounts/filing/transformer/TransformerServiceTest.java
@@ -30,7 +30,7 @@ class TransformerServiceTest {
     }
 
     @Test
-    void testSetupTransaction() {
+    void testSetupTransactionWithCICPackageType() {
         var transaction = new Transaction();
         var transactionId = "transactionId";
         var accountFilingId = "accountFilingId";
@@ -52,6 +52,59 @@ class TransformerServiceTest {
         assertTrue(transaction.getResources().containsKey(uri));
         assertEquals(RESOURCE_KIND, resourceResult.getKind());
         assertEquals(3, resourceResult.getLinks().size());
+        assertEquals(links, resourceResult.getLinks());
+        assertNotNull(resourceResult.getUpdatedAt());
+    }
+
+    @Test
+    void testSetupTransactionWithOverseasPackageType() {
+        var transaction = new Transaction();
+        var transactionId = "transactionId";
+        var accountFilingId = "accountFilingId";
+        var uri = "/transactions/" + transactionId + "/accounts-filing/" + accountFilingId;
+
+        Map<String, String> links = Map.of(
+                "resource", uri,
+                "validation_status", uri + "/validation-status",
+                "costs",uri + "/costs");
+
+        transaction.setId(transactionId);
+        transaction.setResources(new HashMap<>());
+
+        // then
+        service.setupTransactionResources(transaction, accountFilingId, "overseas");
+        Resource resourceResult = transaction.getResources().get(uri);
+
+        assertEquals(1, transaction.getResources().size());
+        assertTrue(transaction.getResources().containsKey(uri));
+        assertEquals(RESOURCE_KIND, resourceResult.getKind());
+        assertEquals(3, resourceResult.getLinks().size());
+        assertEquals(links, resourceResult.getLinks());
+        assertNotNull(resourceResult.getUpdatedAt());
+    }
+
+    @Test
+    void testSetupTransactionWithOtherPackageType() {
+        var transaction = new Transaction();
+        var transactionId = "transactionId";
+        var accountFilingId = "accountFilingId";
+        var uri = "/transactions/" + transactionId + "/accounts-filing/" + accountFilingId;
+
+        Map<String, String> links = Map.of(
+                "resource", uri,
+                "validation_status", uri + "/validation-status");
+
+        transaction.setId(transactionId);
+        transaction.setResources(new HashMap<>());
+
+        // then
+        service.setupTransactionResources(transaction, accountFilingId, "uksef");
+        Resource resourceResult = transaction.getResources().get(uri);
+
+        assertEquals(1, transaction.getResources().size());
+        assertTrue(transaction.getResources().containsKey(uri));
+        assertEquals(RESOURCE_KIND, resourceResult.getKind());
+        assertEquals(2, resourceResult.getLinks().size());
         assertEquals(links, resourceResult.getLinks());
         assertNotNull(resourceResult.getUpdatedAt());
     }
