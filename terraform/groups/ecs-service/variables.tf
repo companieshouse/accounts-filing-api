181e7758947e20caa8af38a3566d2801911fe5e0
lwalker-ch <lwalker@companieshouse.gov.uk>
2024-11-13 17:24:21 +0000

    Updated terraform modules

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -1,20 +1,23 @@
 # ------------------------------------------------------------------------------
 # Environment
 # ------------------------------------------------------------------------------
-variable "environment" {
-  type        = string
-  description = "The environment name, defined in envrionments vars."
-}
 variable "aws_region" {
   default     = "eu-west-2"
   type        = string
   description = "The AWS region for deployment."
 }
+
 variable "aws_profile" {
   default     = "development-eu-west-2"
   type        = string
   description = "The AWS profile to use for deployment."
 }
+
+variable "environment" {
+  type        = string
+  description = "The environment name, defined in envrionments vars."
+}
+
 # ------------------------------------------------------------------------------
 # Docker Container
 # ------------------------------------------------------------------------------
@@ -26,41 +29,53 @@ variable "docker_registry" {
 # ------------------------------------------------------------------------------
 # Service performance and scaling configs
 # ------------------------------------------------------------------------------
+variable "accounts_filing_api_version" {
+  type        = string
+  description = "The version of the accounts filing api container to run."
+}
+
 variable "desired_task_count" {
   type        = number
   description = "The desired ECS task count for this service"
   default     = 1 # defaulted low for dev environments, override for production
 }
+
+variable "max_task_count" {
+  type        = number
+  description = "The maximum number of tasks for this service."
+  default     = 3
+}
+
+variable "min_task_count" {
+  type        = number
+  description = "The minimum number of tasks for this service."
+  default     = 1
+}
+
 variable "required_cpus" {
   type        = number
   description = "The required cpu resource for this service. 1024 here is 1 vCPU"
   default     = 256 # defaulted low for dev environments, override for production
 }
+
 variable "required_memory" {
   type        = number
   description = "The required memory for this service"
   default     = 512 # defaulted low for node service in dev environments, override for production
 }
-variable "use_fargate" {
-  type        = bool
-  description = "If true, sets the required capabilities for all containers in the task definition to use FARGATE, false uses EC2"
-  default     = true
-}
-variable "use_capacity_provider" {
-  type        = bool
-  description = "Whether to use a capacity provider instead of setting a launch type for the service"
-  default     = true
-}
+
 variable "service_autoscale_enabled" {
   type        = bool
   description = "Whether to enable service autoscaling, including scheduled autoscaling"
   default     = true
 }
+
 variable "service_autoscale_target_value_cpu" {
   type        = number
   description = "Target CPU percentage for the ECS Service to autoscale on"
-  default     = 50 # 100 disables autoscaling using CPU as a metric
+  default     = 80 # 100 disables autoscaling using CPU as a metric
 }
+
 variable "service_scaledown_schedule" {
   type        = string
   description = "The schedule to use when scaling down the number of tasks to zero."
@@ -69,6 +84,7 @@ variable "service_scaledown_schedule" {
 
   default     = ""
 }
+
 variable "service_scaleup_schedule" {
   type        = string
   description = "The schedule to use when scaling up the number of tasks to their normal desired level."
@@ -77,9 +93,17 @@ variable "service_scaleup_schedule" {
 
   default     = ""
 }
-variable "accounts_filing_api_version" {
-  type        = string
-  description = "The version of the accounts filing api container to run."
+
+variable "use_capacity_provider" {
+  type        = bool
+  description = "Whether to use a capacity provider instead of setting a launch type for the service"
+  default     = true
+}
+
+variable "use_fargate" {
+  type        = bool
+  description = "If true, sets the required capabilities for all containers in the task definition to use FARGATE, false uses EC2"
+  default     = true
 }
 
 # ----------------------------------------------------------------------
@@ -99,28 +123,33 @@ variable "multilb_cloudwatch_alarms_enabled" {
 # ------------------------------------------------------------------------------
 # Service environment variable configs
 # ------------------------------------------------------------------------------
+variable "account_api_url" {
+  type = string
+}
+
+
+variable "developer_hub_url" {
+  type = string
+}
+
 variable "log_level" {
   default     = "info"
   type        = string
   description = "The log level for services to use: trace, debug, info or error"
 }
+
 variable "ssm_version_prefix" {
   type        = string
   description = "String to use as a prefix to the names of the variables containing variables and secrets version."
   default     = "SSM_VERSION_"
 }
+
 variable "use_set_environment_files" {
   type        = bool
-  default     = false
+  default     = true
   description = "Toggle default global and shared environment files"
 }
-variable "human_log" {
-  type    = string
-  default = "1"
-}
-variable "account_api_url" {
-  type = string
-}
-variable "developer_hub_url" {
-  type = string
-}
+
+
+
+
