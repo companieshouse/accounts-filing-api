1951df5b47ff3390e605dbf235489d6ac7ade0c0
amartin7663 <almartin@companieshouse.gov.uk>
2025-02-20 14:15:03 +0000

    include the cost in the filing, so the refund process picks it up

--- src/test/java/uk/gov/companieshouse/accounts/filing/mapper/FilingGeneratorMapperTest.java
+++ src/test/java/uk/gov/companieshouse/accounts/filing/mapper/FilingGeneratorMapperTest.java
@@ -26,6 +26,8 @@ class FilingGeneratorMapperTest {
     private final static String madeUpDate = "2000-01-01";
     private final static String madeUpDateFormatted = "1 January 2000";
     private final static String accountsType = "01";
+    private final static String CIC_COST = "20.00";
+    private final static String OVERSEAS_COST = "30.00";
     private static List<Map<String, String>> links;
 
     private AccountsFilingEntry createAccountsFilingEntry() {
@@ -49,6 +51,8 @@ class FilingGeneratorMapperTest {
         filingGeneratorMapper = new FilingGeneratorMapper();
         ReflectionTestUtils.setField(filingGeneratorMapper, "bucket", "bucket");
         ReflectionTestUtils.setField(filingGeneratorMapper, "scheme", "scheme");
+        ReflectionTestUtils.setField(filingGeneratorMapper, "cicCost", CIC_COST);
+        ReflectionTestUtils.setField(filingGeneratorMapper, "overseasCost", OVERSEAS_COST);
         links = createLinks();
     }
 
@@ -80,7 +84,24 @@ class FilingGeneratorMapperTest {
         assertEquals(PackageTypeApi.OVERSEAS.toString(), filingApi.getData().get("package_type"));
         assertEquals(accountsType, filingApi.getData().get("accounts_type"));
         assertEquals(createLinks(), filingApi.getData().get("links"));
+        assertEquals(OVERSEAS_COST, filingApi.getCost());
         assertNull(filingApi.getData().get("period_end_on"));
+    }
+
+    @Test
+    void testMapToFilingApiWhenPackageTypeIsCIC() {
+        accountsFilingEntry = createAccountsFilingEntry();
+        accountsFilingEntry.setPackageType(PackageTypeApi.CIC);
+        FilingApi filingApi = filingGeneratorMapper.mapToFilingApi(accountsFilingEntry);
+        assertEquals("Package accounts made up to " + madeUpDateFormatted, filingApi.getDescription());
+        assertEquals("AUDITED FULL", filingApi.getDescriptionIdentifier());
+        assertEquals(Collections.singletonMap("made up date", madeUpDate), filingApi.getDescriptionValues());
+        assertEquals("accounts", filingApi.getKind());
+        assertEquals(PackageTypeApi.CIC.toString(), filingApi.getData().get("package_type"));
+        assertEquals(accountsType, filingApi.getData().get("accounts_type"));
+        assertEquals(createLinks(), filingApi.getData().get("links"));
+        assertEquals(madeUpDate, filingApi.getData().get("period_end_on"));
+        assertEquals(CIC_COST, filingApi.getCost());
     }
 
     @Test
